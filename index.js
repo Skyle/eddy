f4aefb4f6f44cd6c646d11a013787b0f9b9232ef
Philipp Friedrich <freezer05@googlemail.com>
2022-02-16 16:10:27 +0100

    geht

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
 const spielfeld = document.getElementById("spielfeld");
+const richtungsMoeglichkeiten = ["N", "O", "S", "W"];
+
 const figur = {
   element: document.createElement("div"),
-  x: window.innerWidth / 2,
-  y: window.innerHeight / 2,
-  ausrichtung: "N",
+  x: window.innerWidth / 2 - 12,
+  y: window.innerHeight / 2 - 12,
+  ausrichtung: richtungsMoeglichkeiten[0],
 };
 figur.element.style.width = "24px";
 figur.element.style.height = "24px";
 figur.element.style.backgroundColor = "black";
-translateFigur(figur.x, figur.y);
-figur.x += 100;
-figur.y += 100;
+moveForward();
+moveForward();
+moveForward();
+moveForward();
+moveForward();
+moveForward();
+console.log(figur);
+rotation("L");
+console.log(figur);
+rotation("R");
+console.log(figur);
+rotation("R");
+rotation("R");
+console.log(figur);
+
 translateFigur(figur.x, figur.y);
 
+const inputString = "GGGRGLGGRLGRLLGGRGR";
 spielfeld.append(figur.element);
+
+function rotation(richtung) {
+  const richtungsIndex = richtungsMoeglichkeiten.indexOf(figur.ausrichtung);
+  if (richtung === "L") {
+    if (richtungsIndex === 0) {
+      figur.ausrichtung =
+        richtungsMoeglichkeiten[richtungsMoeglichkeiten.length - 1];
+    } else {
+      figur.ausrichtung = richtungsMoeglichkeiten[richtungsIndex - 1];
+    }
+  } else if (richtung === "R") {
+    if (richtungsIndex === richtungsMoeglichkeiten.length - 1) {
+      figur.ausrichtung = richtungsMoeglichkeiten[0];
+    } else {
+      figur.ausrichtung = richtungsMoeglichkeiten[richtungsIndex + 1];
+    }
+  }
+}
 
 function moveForward() {
   if (figur.ausrichtung === "N") {
-    figur.y - 24;
+    figur.y -= 24;
   } else if (figur.ausrichtung === "S") {
+    figur.y += 24;
+  } else if (figur.ausrichtung === "W") {
+    figur.x -= 24;
+  } else if (figur.ausrichtung === "O") {
+    figur.x += 24;
   }
 }
 
