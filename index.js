9fb7b9c6059bf9c8d0a88240a5e11b1c458f5caa
Philipp Friedrich <freezer05@googlemail.com>
2022-02-16 15:49:28 +0100

    jaaa

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const figur = {
   element: document.createElement("div"),
   x: window.innerWidth / 2,
   y: window.innerHeight / 2,
+  ausrichtung: "N",
 };
 figur.element.style.width = "24px";
 figur.element.style.height = "24px";
 figur.element.style.backgroundColor = "black";
-console.log(figur);
-const translateString = "translate(" + figur.x + "px, " + figur.y + "px);";
-console.log(translateString);
-figur.element.style.transform =
-  "translate(" + figur.x + "px, " + figur.y + "px);";
+translateFigur(figur.x, figur.y);
+figur.x += 100;
+figur.y += 100;
+translateFigur(figur.x, figur.y);
+
 spielfeld.append(figur.element);
+
+function moveForward() {
+  if (figur.ausrichtung === "N") {
+    figur.y - 24;
+  } else if (figur.ausrichtung === "S") {
+  }
+}
+
+function translateFigur(x, y) {
+  figur.element.style.transform = "translate(" + x + "px ," + y + "px)";
+}
